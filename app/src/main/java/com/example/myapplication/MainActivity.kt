f293f1b52952a4cb3b223d293ca6ddafae0afc0f
Joscan4676 <112806522+Joscan4676@users.noreply.github.com>
2023-12-07 22:17:24 +0600

    hotel1221

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,17 +1,26 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 
 
 class MainActivity : AppCompatActivity() {
+    private fun Button(): Any {
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Button buttonBack = (Button()) findViewbyId (R.id. buttonBack);
+        buttonBack.setOnClicklistener（thisl；
+
     }
-    public void startnew(Viev)
 
 
-}
 
+`
